9f2a314c40eb4899e30b61bc3d18331547928bda
aadarshchhetry <aadarsh.chhetry00@gmail.com>
2019-01-17 00:24:42 +0530

    last update to contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
 	  	name = params[:contact][:name]
 	  	email = params[:contact][:email]
 	  	body = params[:contact][:comments]
-	  	ContactMailer.contact_email(name,email,body).deliver
+	  	ContactMailer.contact_email(name, email, body).deliver
 	     flash[:success] = "Sent succesfully"
 	     redirect_to new_contact_path
       else
