adcb44f62cb6e59f38184f2e24a1d2d4a2a1a67d
aadarshchhetry <aadarsh.chhetry00@gmail.com>
2019-01-18 00:31:04 +0530

    Added comments in Model and Controller

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+# To check whether the entries are completely filled or not, 
+# by the user 
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, presence: true
